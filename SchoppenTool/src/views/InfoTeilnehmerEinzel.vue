83eceac9650b1772186f85e3c4e569a410a545a1
cleangraphx <103975051+cleangraphx@users.noreply.github.com>
2025-12-27 14:36:39 +0100

    InfoTeilnehmerEinzel.vue
    Aufbau / Konzept erstellt für die Einzelansicht für teilnehmer

---
+++ SchoppenTool/src/views/InfoTeilnehmerEinzel.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div class="innercontainer">
+    <div>
+      <h2>Teilnehmeransicht:</h2>
+      <h3>Bearbeiten</h3>
+    </div>
+    <div class="divider" />
+    <h2>{{ teilnehmer.vorname + ' ' + teilnehmer.nachname }}</h2>
+    <p>{{ teilnehmer.geschlecht }}</p>
+    <p>{{ teilnehmer.geburtstag }}</p>
+    <p>{{ teilnehmer.adresse }}</p>
+    <p>{{ teilnehmer.email }}</p>
+    <h3>Besuchte Freizeiten:</h3>
+    <ul>
+      <li>noch keine Freizeiten besucht</li>
+      <li>...</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
