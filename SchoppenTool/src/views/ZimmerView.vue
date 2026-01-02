af2f985a1043b82b086f7920b15a70b3df3333e5
Jonathan Hörstrup <cleangraphx@outlook.de>
2026-01-02 15:45:54 +0100

    Added ZimmerView.vue
    Created basic page structure
    Made notes regarding the data structure required for ZimmerView.vue

---
+++ SchoppenTool/src/views/ZimmerView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="innercontainer">
+    <div class="zimmer" v-for="zimmer in zimmerDaten" :key="zimmer.ID">
+      <h2>{{  zimmer.zimmername  }}</h2>
+      <div class="zimmerconfig">
+        <button class="zimmerconfig switchbutton aktiv">Aktiviert</button>
+        <button class="zimmerconfig switchbutton geschlecht-m">Männer / Jungs</button>
+        <input type="text" class="zimmerconfig" placeholder="{{  zimmer.maximal  }}">
+      </div>
+      <div class="zimmerteilnehmer">
+        <ol class="zimmerteilnehmer">
+          <li class="zimmerteilnehmer" v-for="teilnehmer in zimmer.teilnehmer">{{ teilnehmer.name }}</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
