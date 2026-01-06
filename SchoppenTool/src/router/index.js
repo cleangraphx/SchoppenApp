2cbc3b4417ac206f3fed16f29e19ad4ec7d4d9e8
Jonathan Hörstrup <cleangraphx@outlook.de>
2026-01-07 00:47:53 +0100

    installed npm package

--- SchoppenTool/src/router/index.js
+++ SchoppenTool/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import ViewView from '@/views/ViewView.vue'
 import InputTeilnehmerView from '@/views/InputTeilnehmerView.vue'
@@ -9,12 +8,6 @@ import ZimmerView from '@/views/ZimmerView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-	    meta: { requiresAuth: true}
-    },
 	  {
 		  path: '/login',
 		  name: 'login',
